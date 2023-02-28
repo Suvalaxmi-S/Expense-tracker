efd17e2b78c5a32829978e8ebf13231e169ad778
SUVALAXMI S <109423761+Suvalaxmi-S@users.noreply.github.com>
2023-02-28 20:08:05 +0530

    Add files via upload

---
+++ Counter.jsx
@@ -0,0 +1,22 @@
+import React,{useEffect, useState} from "react";
+const Counter=()=>{
+    const [number,setNumber]=useState(0);
+    const [text,setText]=useState('hello')
+    useEffect(()=>{
+        console.log('call from global console');
+        setText(Math.random());
+    },[number]);
+    
+    //setText(Math.random());
+    return (<div>
+        <div>{number}</div>
+        
+        <div>NEW APP</div>
+        
+        <div>{text}</div>
+        <button onClick={()=> setNumber(Math.random())}>Update Number</button>
+        <button onClick={()=> setText(Math.random())}>Update Text</button>
+
+    </div>);
+}
+export default Counter;
